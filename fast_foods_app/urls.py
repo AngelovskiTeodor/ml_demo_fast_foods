c2ef46085528a5403d859d78dba70eff1ee7591c
Teodor Angelovski <angelovski.teo@outlook.com>
2023-05-09 00:18:46 +0200

    Implemented Routes and Added HTML Templates

---
+++ fast_foods_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('train', views.train, name="train"),
+    path('predict', views.predict, name="predict"),
+    path('result', views.result, name="result")
+]
--- fast_foods_app/views.py
+++ fast_foods_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+@csrf_exempt
+def index(request):
+    context = {}
+    return render(request, "fast_foods/index.html", context)
+
+def train(request):
+    accuracy = 0
+    context = {"accuracy": accuracy}
+    return render(request, "fast_foods/train.html", context)
+
+def predict(request):
+    context = {}
+    return render(request, "fast_foods/predict.html", context)
+
+def result(request):
+    context = {}
+    return render(request, "fast_foods/result.html", context)
